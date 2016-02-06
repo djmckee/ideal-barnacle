50f95a7179b110b9bd11e52692523cfca762b288
Dylan McKee <dylan@djmckee.co.uk>
2016-02-06 13:56:39 +0000

    Transitioned int to enum.

--- src/trading_game/TradingStrategy.java
+++ src/trading_game/TradingStrategy.java
@@ -7,51 +7,55 @@ import game.BaseTradingStrategy;
 import exceptions.InsufficientFundsException;
 import exceptions.InsufficientSharesException;
 
+enum TradeStatus {
+	BOUGHT,
+	SOLD
+}
+
 public class TradingStrategy extends BaseTradingStrategy {
-			
+
 	public TradingStrategy (){
 		// Initialise any variables needed.
-		
+
 	}
-	
+
 	@Override
 	public DailyOutput makeDailyTrade(DailyInput input) throws InsufficientFundsException, InsufficientSharesException {
-		
+
 		// Use the trading manager to make trades based on input.
-		
+
 		DailyOutput output;
-		int sold;//0 if nothing 1 if sold 2 if bought
-		
+		TradeStatus status;
+
 		double delta = input.getClose() - input.getOpen();
-		
+
 		if (delta < -1) {
 			// share going down
-						output = tradingManager.sellAllShares(input);
-						//testing
-						sold = 1;
+			output = tradingManager.sellAllShares(input);
+			//testing
+			status = TradeStatus.SOLD;
 		} else {
 			// share going up
 			output = tradingManager.buyMaxNumberOfShares(input);
 			//testing
-			sold = 2;
+			status = TradeStatus.BOUGHT;
+
 		}
-		
-		
+
+
 		//test stuff
 		System.out.print("DAY " + input.getDay() + ": opening: " + input.getOpen() + ", close: " + input.getClose()
-				+ ", high: " + input.getHigh() + ", low: " + input.getLow());
-		switch(sold){
-		case 2:
+		+ ", high: " + input.getHigh() + ", low: " + input.getLow());
+		switch(status){
+		case BOUGHT:
 			System.out.println("   BOUGHT");
 			break;
-		case 1:
+		case SOLD:
 			System.out.println("   SOLD");
 			break;
-		case 0:
-			System.out.println();
-			break;
+
 		}
-		
+
 		return output;
 	}
 }
