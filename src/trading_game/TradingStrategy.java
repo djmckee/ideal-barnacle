5de9b9964e4dcee30c85f28754dcd1bb855546e6
Matt Tennison <contact@matttennison.com>
2016-02-06 13:54:42 +0000

    Little code tidy up

--- src/trading_game/TradingStrategy.java
+++ src/trading_game/TradingStrategy.java
@@ -8,38 +8,38 @@ import exceptions.InsufficientFundsException;
 import exceptions.InsufficientSharesException;
 
 public class TradingStrategy extends BaseTradingStrategy {
-			
+
 	public TradingStrategy (){
 		// Initialise any variables needed.
-		
+
 	}
-	
+
 	@Override
 	public DailyOutput makeDailyTrade(DailyInput input) throws InsufficientFundsException, InsufficientSharesException {
-		
+
 		// Use the trading manager to make trades based on input.
-		
+
 		DailyOutput output;
 		int sold;//0 if nothing 1 if sold 2 if bought
-		
+
 		double delta = input.getClose() - input.getOpen();
-		
+
 		if (delta < -1) {
 			// share going down
-						output = tradingManager.sellAllShares(input);
-						//testing
-						sold = 1;
+			output = tradingManager.sellAllShares(input);
+			//testing
+			sold = 1;
 		} else {
 			// share going up
 			output = tradingManager.buyMaxNumberOfShares(input);
 			//testing
 			sold = 2;
 		}
-		
-		
+
+
 		//test stuff
 		System.out.print("DAY " + input.getDay() + ": opening: " + input.getOpen() + ", close: " + input.getClose()
-				+ ", high: " + input.getHigh() + ", low: " + input.getLow());
+		+ ", high: " + input.getHigh() + ", low: " + input.getLow());
 		switch(sold){
 		case 2:
 			System.out.println("   BOUGHT");
@@ -51,7 +51,7 @@ public class TradingStrategy extends BaseTradingStrategy {
 			System.out.println();
 			break;
 		}
-		
+
 		return output;
 	}
 }
